ef6ad88668b7a1794e5fe38579fde4d5383dafa5
NOAH DEMIAN ROTBARD <52096691@ort>
2023-10-02 08:15:09 -0300

    v1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP09_LoginMVCyJavaScript.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Account(){
+
+        
+    }
+
+}
---
+++ Models/BD.cs
@@ -0,0 +1,10 @@
+using System.Data.SqlClient; 
+using Dapper; 
+
+public class BD{
+    private static string _connectionString = @"Server=localhost;DataBase=tp09JavaScriptYMVC;Trusted_Connection=True;";
+
+
+
+
+}
